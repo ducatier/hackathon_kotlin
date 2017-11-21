a151de40221d8b0bfbe45d22945e36ba4397304d
geertjan <geertjan.sterenborg@duo.nl>
2017-11-21 14:50:48 +0100

    test

---
+++ src/main/kotlin/nl/cgi/hackaton/domain/Movie.kt
@@ -0,0 +1,16 @@
+package nl.cgi.hackaton.domain
+
+
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Movie(
+        var firstName: String = "",
+        var lastName: String = "",
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        var id: Long = 0
+)
